f56222a63e60bb09b698ccaf44c3d0298b39dc4a
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-05-31 19:16:32 -0300

    Update Data.php

--- app/code/community/UltraDev/MediaSync/Helper/Data.php
+++ app/code/community/UltraDev/MediaSync/Helper/Data.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 class UltraDev_MediaSync_Helper_Data extends Mage_Core_Helper_Abstract
 {
     public function isEnabled()
@@ -8,6 +8,13 @@ class UltraDev_MediaSync_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function getSkippedFolders()
     {
-        return ['cache', 'css_secure', 'css', 'js', 'tmp'];
+        return ['cache', 'css_secure', 'css', 'js', 'tmp', 'customer', 'downloadable', 'xmlconnect', 'theme', 'header'];
+    }
+
+    public function isSkippedFile($relativePath)
+    {
+        $skippedFiles = ['.htaccess', '.htpasswd', 'php.ini', '.env'];
+        $filename = basename($relativePath);
+        return in_array($filename, $skippedFiles);
     }
 }
